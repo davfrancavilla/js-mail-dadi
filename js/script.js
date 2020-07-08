fd6ef1128b3a2bc8c3e0a54864402a7ed35bde7f
Davide Francavilla <davfrancavilla@gmail.com>
2020-07-08 15:37:29 +0200

    Controllo email funzionante

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var emails = ["prova@prova.it", "ciao@ciao.it", "davide@davide.it"];
+
+document.getElementById("button").addEventListener("click", check); 
+
+function check() {
+    bool = false;
+    var email = document.getElementById("email").value;
+    for (i in emails) {
+        document.getElementById("result").style.display = "none";
+        if (emails[i] == email) {
+            bool = true;
+        }
+    }
+    if (bool) {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("result").style.color = "green";
+        document.getElementById("result").innerHTML = "La tua mail è presente nell'elenco."
+    } else {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("result").style.color = "red";
+        document.getElementById("result").innerHTML = "La tua mail non è presente nell'elenco."
+    }
+
+}
