137eae73c347463a18928cae6d6e7803495741ac
Davide Francavilla <davfrancavilla@gmail.com>
2020-07-08 16:03:58 +0200

    Aggiunto elenco email nella pagina

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var emails = ["prova@prova.it", "ciao@ciao.it", "davide@davide.it"];
 
+
 document.getElementById("button").addEventListener("click", check); 
 
 function check() {
@@ -14,11 +15,20 @@ function check() {
     if (bool) {
         document.getElementById("result").style.display = "block";
         document.getElementById("result").style.color = "green";
-        document.getElementById("result").innerHTML = "La tua mail è presente nell'elenco."
+        document.getElementById("result").innerHTML = "La tua mail è presente nell'elenco.";
     } else {
         document.getElementById("result").style.display = "block";
         document.getElementById("result").style.color = "red";
-        document.getElementById("result").innerHTML = "La tua mail non è presente nell'elenco."
+        document.getElementById("result").innerHTML = "La tua mail non è presente nell'elenco.";
     }
 
+}
+
+
+for (i in emails) {
+    console.log(emails[i]);
+    var p = document.createElement("P");
+    // p.appendChild(emails[i]);
+    p.innerHTML += (emails[i])
+    document.getElementById("emails").appendChild(p);
 }
