14a0ac8b9ccd30eb2cdff76bbd315b550e3480f8
Davide Francavilla <davfrancavilla@gmail.com>
2020-07-09 17:07:01 +0200

    Aggiornato con consegna del 09-07

--- js/script.js
+++ js/script.js
@@ -1,34 +1,48 @@
-var emails = ["prova@prova.it", "ciao@ciao.it", "davide@davide.it"];
+var user = Math.floor(Math.random() * 6 + 1);
+var computer = Math.floor(Math.random() * 6 + 1);
 
+document.getElementById('user').innerHTML = "Numero utente: " + user;
+document.getElementById('computer').innerHTML = "Numero computer: " + computer;
 
-document.getElementById("button").addEventListener("click", check); 
-
-function check() {
-    bool = false;
-    var email = document.getElementById("email").value;
-    for (i in emails) {
-        document.getElementById("result").style.display = "none";
-        if (emails[i] == email) {
-            bool = true;
-        }
-    }
-    if (bool) {
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "green";
-        document.getElementById("result").innerHTML = "La tua mail è presente nell'elenco.";
-    } else {
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "red";
-        document.getElementById("result").innerHTML = "La tua mail non è presente nell'elenco.";
-    }
-
+if (user > computer) {
+    document.getElementById('vincitore').innerHTML = "Hai vinto!";
+} else if (computer > user) {
+    document.getElementById('vincitore').innerHTML = "Il computer ha vinto";
+} else {
+    document.getElementById('vincitore').innerHTML = "Pareggio";
 }
 
-
-for (i in emails) {
-    console.log(emails[i]);
-    var p = document.createElement("P");
-    // p.appendChild(emails[i]);
-    p.innerHTML += (emails[i])
-    document.getElementById("emails").appendChild(p);
-}
+// var emails = ["prova@prova.it", "ciao@ciao.it", "davide@davide.it"];
+//
+//
+// document.getElementById("button").addEventListener("click", check);
+//
+// function check() {
+//     bool = false;
+//     var email = document.getElementById("email").value;
+//     var i;
+//     for (i in emails) {
+//         document.getElementById("result").style.display = "none";
+//         if (emails[i] == email) {
+//             bool = true;
+//         }
+//     }
+//     if (bool) {
+//         document.getElementById("result").style.display = "block";
+//         document.getElementById("result").style.color = "green";
+//         document.getElementById("result").innerHTML = "La tua mail è presente nell'elenco.";
+//     } else {
+//         document.getElementById("result").style.display = "block";
+//         document.getElementById("result").style.color = "red";
+//         document.getElementById("result").innerHTML = "La tua mail non è presente nell'elenco.";
+//     }
+//
+// }
+//
+//
+// for (i in emails) {
+//     console.log(emails[i]);
+//     var p = document.createElement("P");
+//     p.innerHTML += (emails[i])
+//     document.getElementById("emails").appendChild(p);
+// }
